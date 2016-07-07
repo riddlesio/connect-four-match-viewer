8be2cebfde743b3b900a6127e7bbd973d04e8721
Joey <joey@starapple.nl>
2016-07-07 09:45:45 +0200

    Adds png background. Adds SVG images to gameviewer defs [t: 2h]

--- src/js/view/FieldView.jsx
+++ src/js/view/FieldView.jsx
@@ -1,18 +1,14 @@
 import React        from 'react';
 import createView   from 'omniscient';
 import Cell         from './Cell.jsx';
-const _ = require('lodash');
-
-// import _            from 'lodash';
+import _            from 'lodash';
 
 const FieldView = createView('FieldView', function (state) {
 
     const { cells } = state;
 
     return <g className="Connect4Game-gameView" >
-            <g dangerouslySetInnerHTML={{
-                __html: `<use x="50" y="135" xlink:href="#connect4-background" />`,
-            }} />
+            <use x="50" y="135"  width="1100" height="500" xlinkHref="#connect4-background" />
             <g className="Connect4Game-grid">
                 { _.map(cells, Cell) }
             </g>
